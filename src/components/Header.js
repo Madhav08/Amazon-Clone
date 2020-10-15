4993301a9c28f0bc13cab219bf8d95b13cc841ea
unknown <madhav.sharma1201@gmail.com>
2020-10-15 12:54:14 +0530

    completed add to basket functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,21 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { Link } from "react-router-dom";
+import { useStateValue } from "../StateProvider";
 
 function Header() {
+
+  const [{basket}, dispatch] = useStateValue();
+
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG25.png"
-      />
+      <Link to="/">
+          <img
+            className="header__logo"
+            src="http://pngimg.com/uploads/amazon/amazon_PNG25.png"
+          />
+      </Link>
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
@@ -32,10 +39,12 @@ function Header() {
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
+        <Link to="/checkout">
         <div className="header__optionsBasket">
           <ShoppingCartIcon />
-          <span className="header__optionLineTwo header__basketCount">0 </span>
+  <span className="header__optionLineTwo header__basketCount">{basket.length}</span>
         </div>
+        </Link>
       </div>
     </div>
   );
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useStateValue } from "../StateProvider";
 import "./Product.css";
 
 function Product({ title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  console.log("Basket >>>", basket)
+  const addToBasket = () => {
+    
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -14,12 +30,16 @@ function Product({ title, image, price, rating }) {
           {Array(rating)
             .fill()
             .map((_, i) => (
-              <p>⭐</p>
+              <p>
+                
+                <span role="img">⭐</span>
+              
+              </p>
             ))}
         </div>
       </div>
-      <img src={image} alt="Product Image" />
-      <button>Add To Basket</button>
+      <img src={image} alt="Product" />
+      <button onClick={addToBasket}>Add To Basket</button>
     </div>
   );
 }
