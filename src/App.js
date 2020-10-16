546551affa3d0c84de351ed0c044ff1248783b35
unknown <madhav.sharma1201@gmail.com>
2020-10-16 18:31:57 +0530

    completed signup functionlity

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./components/Checkout";
 import Login from "./components/Login";
+import Register from "./components/Register";
+import { auth } from "./firebase";
 
 function App() {
+
+  // useEffect(() => {
+  //   return () => {
+  //     auth.onAuthStateChanged()
+  //   }
+  // }, [])
+
   return (
     <Router>
         <div className="app">
@@ -14,6 +23,10 @@ function App() {
           <Switch>
           <Route path="/login">
               <Login />
+            </Route>
+
+            <Route path="/register">
+              <Register />
             </Route>
 
           <Route path="/checkout">
